e18b5c3e69a01e588c09cf61d420576d2ff0983c
suw <susususuwang@gmail.com>
2022-05-07 00:56:16 -0400

    update main.py

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def addressMatching(sql_MLS: str, sql_ASSESSOR: str) -> pd.DataFrame:
     df_MLS = read_sql(sql_MLS, ['CC_PROPERTY_ADDR_DISPLAY_1'])
     df_ASSESSOR = read_sql(sql_ASSESSOR, ['SITUSFULLSTREETADDRESS'])
 
+    uq_address_ASSESSOR = len(df_ASSESSOR['SITUSFULLSTREETADDRESS'].unique())
+
+
     #standardize the address
     df_MLS = standardizeAddress(df_MLS, 'CC_PROPERTY_ADDR_DISPLAY_1', ignore=['OccupancyType', 'OccupancyIdentifier'])
     df_ASSESSOR = standardizeAddress(df_ASSESSOR, 'SITUSFULLSTREETADDRESS',
@@ -35,6 +38,9 @@ def addressMatching(sql_MLS: str, sql_ASSESSOR: str) -> pd.DataFrame:
     matched_df, remaining = match(df_ASSESSOR[df_ASSESSOR['placekey_new'] != 'Invalid address'],
                                   df_MLS[df_MLS['placekey_new'] != 'Invalid address'], df1_key='placekey_new',
                                   df2_key='placekey_new')
+    #check percentage
+    uq_address_matched = len(matched_df['SITUSFULLSTREETADDRESS_x'].unique())
+    print(uq_address_matched/uq_address_ASSESSOR*100,'% of unique addressses in the base table are matched')
     ##use a pool list to hold our results
     pool = []
     pool.append(matched_df)
@@ -47,9 +53,13 @@ def addressMatching(sql_MLS: str, sql_ASSESSOR: str) -> pd.DataFrame:
                                   ['SITUSFULLSTREETADDRESS', 'CC_PROPERTY_ADDR_DISPLAY_1'],
                                    whether_name_list_1 = ['OWNER1LASTNAME', 'OWNER1FIRSTNAME', 'OWNER1MIDDLENAME']))
 
-    matched_result = pd.concat(pool, ignore_index=True)
+    matched_df = pd.concat(pool, ignore_index=True)
 
-    return matched_result
+    #check percentage
+    uq_address_matched = len(matched_df['SITUSFULLSTREETADDRESS_x'].unique())
+    print(uq_address_matched/uq_address_ASSESSOR*100,'% of unique addressses in the base table are matched')
+
+    return matched_df
 
 
 if __name__ == '__main__':
